4027225a6857434b3097b5182a463c83ee0a18e6
Alex Ford <gordion42@gmail.com>
2016-09-10 16:37:18 -0600

    A more working version

--- init.lua
+++ init.lua
@@ -1,15 +1,76 @@
 print("Ambience 0.0.0 loaded! Wubba lubba dub dub!")
 
-local music_handle = nil -- TODO: Make these per player
-local music_gain = 1
-local sound_handle = nil
-local sound_gain = 1
+local MUSICVOLUME = 1
+local SOUNDVOLUME = 1
 
-local function play_music(filename)
-	if music_handle ~= nil then
-		minetest.sound_stop(music_handle)
+local volume = {}
+local music = {}
+local sounds = {}
+local world_path = minetest.get_worldpath()
+
+
+local function load_volumes()
+    local file, err = io.open(world_path.."/ambience_volumes", "r")
+    if err then
+        return 
+    end
+    for line in file:lines() do
+        local config_line = string.split(line, ":")
+        volume[config_line[1]] = {music=config_line[2],sound=config_line[3]}
+    end
+    file:close()
+end
+
+load_volumes()
+
+local function play_music(player,filename)
+	local db = volume[player:get_player_name()]
+	if db.music_handle ~= nil then
+		minetest.sound_stop(db.music_handle)
 	end
-	music_handle = minetest.sound_play(filename, {
+	db.music_handle = minetest.sound_play(filename, {
+		to_player = player:get_player_name(),
 		gain = 1.0,
 	})
 end
+
+minetest.register_on_joinplayer(function(player)
+    if volume[player:get_player_name()] == nil then
+        volume[player:get_player_name()] = {music=MUSICVOLUME, sound=SOUNDVOLUME}
+    end
+    volume[player:get_player_name()].music_handle = nil
+    volume[player:get_player_name()].sound_handle = nil
+end)
+minetest.register_chatcommand("volume", {
+    description = "View sliders to set sound a music volume",
+    func = function(name,param)
+        minetest.show_formspec(name, "ambience:volume",
+            "size[6,3.5]" ..
+            "label[0,0.5;Music]" ..
+            "scrollbar[0,1;5.8,0.4;horizontal;music;" .. volume[name].music * 1000 .. "]" ..
+            "label[0,1.5;Sound]" ..
+            "scrollbar[0,2;5.8,0.4;horizontal;sound;" .. volume[name].sound * 1000 .. "]" ..
+            "button_exit[2,2.8;2,0.8;quit;Done]"
+            )
+    end,
+})
+minetest.register_on_player_receive_fields(function(player, formname, fields)
+    if formname ~= "ambience:volume" then
+        return false
+    end
+    minetest.log(dump(fields))
+    if fields.quit ~= "true" then
+        volume[player:get_player_name()].music = tonumber(string.split(fields.music,":")[2]) / 1000
+        volume[player:get_player_name()].sound = tonumber(string.split(fields.sound,":")[2]) / 1000
+    end
+    if fields.quit then
+        local file, err = io.open(world_path.."/ambience_volumes", "w")
+        if not err then
+            for item in pairs(volume) do
+                file:write(item..":"..volume[item].music..":"..volume[item].sound.."\n")
+            end
+            file:close()
+        end
+    end
+    return true
+end)
