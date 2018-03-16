5e84abe51e90295e2ce6dd54ca16b3d764c2d3d4
Alex Ford <gordion42@gmail.com>
2018-03-16 16:05:45 -0600

    Add more of the boilerplate

--- init.lua
+++ init.lua
@@ -6,15 +6,19 @@ local storage = minetest.get_mod_storage()
 
 local volume = {}
 local music = {}
+local music_config = {}
 local sounds = {}
 local world_path = minetest.get_worldpath()
 
 
 local function load_volumes()
     local volumes = storage:get_string("volumes")
+    if volumes == "" then
+        return
+    end
     local playerVolumes = string.split(volumes, ";")
 
-    for line in playerVolumes do
+    for _,line in pairs(playerVolumes) do
         local config_line = string.split(line, ":")
         volume[config_line[1]] = {music=config_line[2],sound=config_line[3]}
     end
@@ -22,14 +26,32 @@ end
 
 local function load_config()
     -- Load /ambience_music/config and /ambience_sounds/config
+    local sounds_config_raw = minetest.get_dir_list(world_path.."/ambience_sounds/config/", false)
+    for v,i in pairs(minetest.get_dir_list(world_path.."/ambience_music/config/", false)) do
+        print(world_path.."/ambience_music/config/"..i)
+        local f = io.open(world_path.."/ambience_music/config/"..i, "r")
+        local c = f:read("*a")
+        music_config[i] = minetest.parse_json(c)
+    end
 end
 
 local function load_music()
-    music = minetest.get_dir_list(world_path.."/ambience_music", "r")
+    music = minetest.get_dir_list(world_path.."/ambience_music", false)
+    for i,v in pairs(music) do
+        music[i] = string.split(v,".")[1] -- Strip the extension
+    end
 end
 
 local function load_sounds()
-    sounds = minetest.get_dir_list(world_path.."/ambience_sounds", "r")
+    sounds = minetest.get_dir_list(world_path.."/ambience_sounds", false)
+end
+
+local function can_play(name, player)
+    if music_config[name] == nil then
+        return false
+    end
+    local foo = music_config[name].conditions
+    return true
 end
 
 
@@ -69,6 +91,13 @@ minetest.register_chatcommand("volume", {
             )
     end,
 })
+
+minetest.register_chatcommand("poop", {
+    func = function(name,param)
+        print(get_biome(minetest.get_player_by_name(name):getpos()))
+    end
+})
+
 minetest.register_on_player_receive_fields(function(player, formname, fields)
     if formname ~= "ambience:volume" then
         return false
@@ -91,7 +120,7 @@ end)
 
 local delay = 0
 minetest.register_globalstep(function(dtime)
-delay += dtime
+delay = delay + dtime
 if delay < 5 then
     return
 end
@@ -102,6 +131,13 @@ delay = 0
 
         if volume[player:get_player_name()].music_handle == nil then -- only play one song at a time
             -- Pick a song, roll the dice
+            for _,i in ipairs(music) do -- Let's determine availability, then apply randomness
+                if can_play(i,player) and math.random(10) == 5 then
+                    print(i)
+                    local p_name = player:get_player_name()
+                    volume[p_name].music_handle = minetest.sound_play(i,{gain= volume[p_name].music, to_player=p_name})
+                end
+            end
         end
 
         -- Somehow decide on ambient sounds (these can overlap)
